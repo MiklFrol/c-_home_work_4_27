7f777c0f87d7841df7b7b42d420c8a487e272038
MiklFrol <miklfrol@mail.ru>
2023-01-19 23:53:38 +0300

    Задаача 27 выполнена

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear();
+
+int getUserNumber(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int sumOfDigits(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number % 10; 
+        number = number / 10;
+    }
+    return sum;
+}
+
+int number = getUserNumber("Введите число:");
+int sum = sumOfDigits(number);
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine($"Сумма цифр в числе {number} = {sum}");
